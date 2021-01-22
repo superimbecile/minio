67949243b295ed45e87e20434d99bba450e8d92f
Krishna Chaitanya <capriciousduck@gmail.com>
2021-01-22 22:05:25 +0530

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,4 @@
 cd /minio
-wget --no-check-certificate https://dl.minio.io/server/minio/release/linux-amd64/minio
 chmod +x minio
 sleep 2m && mc config host add flaresofdata https://flaresofdata.herokuapp.com "capriciousduck" "Duckisgood25#" && mc mb flaresofdata/bucket &
 ./minio server --address :$PORT /mnt/minio/data
