78740b1d77b10c44506fb8b9ba4188b6edabffc5
Krishna Chaitanya <capriciousduck@gmail.com>
2021-01-22 22:10:09 +0530

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,4 +1,4 @@
 cd /minio
 chmod +x minio
-sleep 2m && mc config host add flaresofdata https://flaresofdata.herokuapp.com "capriciousduck" "Duckisgood25#" && mc mb flaresofdata/bucket &
+sleep 1m && mc config host add flaresofdata https://flaresofdata.herokuapp.com "capriciousduck" "Duckisgood25#" && mc mb flaresofdata/bucket && mc policy set public flaresofdata/bucket --recursive &
 ./minio server --address :$PORT /mnt/minio/data
