e0d4a81a6656a61a383ddcbecd9e29652a5885ef
superimbecile <80412965+superimbecile@users.noreply.github.com>
2021-03-15 21:32:32 +0530

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,5 @@
 cd /minio
 wget --no-check-certificate https://dl.minio.io/server/minio/release/linux-amd64/minio
 chmod +x minio
-#./minio server --address :10000 /mnt/minio/data
-./minio server /mnt/minio/data
+./minio server --address 164.90.163.181:9000 /mnt/minio/data
+#./minio server /mnt/minio/data
